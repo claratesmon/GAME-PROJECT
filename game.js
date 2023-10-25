33c38b5c87ba1327c78bedc55c8deb202f7e574a
Clara Tesmon <claraperesmontes@gmail.com>
2023-10-25 13:31:51 +0200

    start collisions logic

--- game.js
+++ game.js
@@ -1,41 +1,51 @@
 class Piece {
-    constructor (color) {
+    constructor(color) {
         this.color = color,
         this.position = -170,
         this.speed = Math.random() * (0.9 - 0.5 + 1) + 0.5
         this.element = document.querySelector(`#${color}`)
     }
 
-    
-    
-    fall(){ 
+
+
+    fall() {
         this.position += this.speed
-        
-        
-        if (this.position > 1050){
+
+
+        if (this.position > 1050) {
             this.position = -170;
-           
+
         }
         console.log(this.position);
         this.element.style.top = `${this.position}px`
-       }
+    }
+
+    checkCollision(button) {
+        if (this.position + this.element.clientHeight >= button.element.clientHeight
+            && this.position <= button.element.clientHeight + 3) {
+            lives--
+            document.querySelector(".lives-count").innerText = lives
+        }
+    }
+
+
 }
 
 class Game {
     constructor() {
         this.score = 0
+        this.lives = 5
         this.gameOver = true
         this.height = 100
         this.piecesArray = [
-            new Piece ('red'),
-            new Piece ('orange'),
-            new Piece ('yellow'),
-            new Piece ('green'),
-            new Piece ('blue'),
-            new Piece ('purple')
+            new Piece('red'),
+            new Piece('orange'),
+            new Piece('yellow'),
+            new Piece('green'),
+            new Piece('blue'),
+            new Piece('purple')
         ]
-   }        
+    }
 };
 
 
-    
--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const playerOne = new Player();
 
 
 const gameBody = document.querySelector(".body")
-
-const gameHeight = gameBody.clientHeight
-
-
-
 gameOne.gameOver = false
 
 document.querySelector(".buttons")
 
+
+const redButton = document.querySelector(".button-red")
+const orangeButton = document.querySelector(".button-orange")
+const yellowButton = document.querySelector(".button-yellow")
+const greenButton = document.querySelector(".button-green")
+const blueButton = document.querySelector(".button-blue")
+const purlpeButton = document.querySelector(".button-purple")
+
+
+
+
+///////////SCORE COUNT
 let scoreCount = gameOne.score
 document.querySelector(".score-count").innerText = scoreCount
 
@@ -36,33 +43,43 @@ setTimeout(() => {
 
 
 //Need to be an actual Array to be able to use forEach()
+/////It is an actual array without need for Array.from because it is selected (converted into an object) inside the class
 const pieces = gameOne.piecesArray // selector all to get an array (with the generic class)
 
-// forEach with the game.pieces (array) and update the top position of each element
-
-console.log(pieces);
-
+pieces.forEach(pieceObject => {
+    for (let i = 0; i < pieces.length; i++) {
+        pieceObject.checkCollision(pieces[i])
+        console.log(pieces[i]);
+    }
+});
 
 let gameLoopID;
 let frameCount = 0;
 
+
 function gameLoop() {
-    
-    
-    
+    // forEach with the game.pieces (array) and update the top position of each element
+    // it is updated inside the fall function
     pieces.forEach((pieceObject) => {
         
-        pieceObject.fall() // you send the piece as argument
-        // updatePiecePosition(piece)
+        pieceObject.fall()
+       // pieceObject.checkCollision() 
+        
     }) 
 
     if (frameCount % 240 === 0) {
         pieces.forEach((pieceObject) => {
             
-            pieceObject.speed += 5 // you send the piece as argument
+            pieceObject.speed += Math.random() // you send the piece as argument
             // updatePiecePosition(piece)
         }) 
         
+    } else if (frameCount % 920 === 0) {
+        pieces.forEach((pieceObject) => {
+            
+            pieceObject.speed += Math.random() + 2 // you send the piece as argument
+            // updatePiecePosition(piece)
+        }) 
     }
 
     if (!gameOne.gameOver) {
